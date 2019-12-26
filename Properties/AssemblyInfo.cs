98e27f26e41e6bdb58f556fc84ab4a28b9df272f
Ronaldo France <ronaldo.silva@catolicasc.org.br>
2019-12-26 11:34:22 -0300

    Primeiro Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("TesteAgapys")]
+[assembly: AssemblyDescription("Aplicação de importação de txt, com buscador de termos e retorno do nº da linha e o nome do arquivo")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Agapys")]
+[assembly: AssemblyProduct("TesteAgapys")]
+[assembly: AssemblyCopyright("Copyright ©  Ronaldo France - 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("4a6e97d9-4cab-4e38-ac19-df869ff2bd2e")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ README.md
@@ -0,0 +1,2 @@
+# Teste-Agapys
+Teste Agapys em C#
